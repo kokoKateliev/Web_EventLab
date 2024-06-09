6c54b62dc40c360b15b54c1f87daf15c96dad3e6
Adriana Atanasova <adriana0702@abv.bg>
2024-06-09 18:31:21 +0300

    edit db

--- backend/db/DB.php
+++ backend/db/DB.php
@@ -6,10 +6,10 @@ class DB {
     public function __construct()
     {
         $dbHost = "localhost";
-		$dbName = "`web_eventlab_db";
+		$dbName = "web_eventlab_db";
         $userName = "root";
 		$password = "";
-        $this->conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $userName, $password,
+        $this->connection = new PDO("mysql:host=$dbHost;dbname=$dbName", $userName, $password,
 			[
 				PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
 				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
