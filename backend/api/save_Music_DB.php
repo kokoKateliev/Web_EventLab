9a89ad6e8b0fac772d58b89302482070f08c309e
Adriana Atanasova <adriana0702@abv.bg>
2024-06-11 21:03:30 +0300

    save music in db

--- backend/api/save_Music_DB.php
+++ backend/api/save_Music_DB.php
@@ -5,9 +5,13 @@ session_start();
 require_once "../db/DB.php";
 require_once "../models/Music.php";
 
-function saveMusic($connection, $eventId, $title, $musicURL) {
-    $username = $_SESSION['username'];
+function getSenderID($connection){
+    if (!isset($_SESSION['username'])) {
+        throw new Exception("Няма логнат потребител!");
+    }
 
+    $username = $_SESSION['username'];
+    
     $sql = "SELECT 
                u.id
             FROM 
@@ -18,22 +22,50 @@ function saveMusic($connection, $eventId, $title, $musicURL) {
     $query = $connection->prepare($sql);
     $query->execute([$username]);
 
-    while($row = $query->fetch(PDO::FETCH_ASSOC)){ 
-        $loggedUserID = $row['id'];
-    } 
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+    if ($row === false) {
+        throw new Exception("Потребителят не е намерен.");
+    }
 
-    $music = new Music(null, $loggedUserID, $title, $musicURL, $eventId)
+    $loggedUserID = $row['id'];
+
+    return $loggedUserID;
 }
 
-$phpInput = json_decode(file_get_contents('php://input'), true);
-$EventID = $phpInput['eventId']; 
-$title = $phpInput['name']; 
-$musicURL = basename($phpInput['musicURL']);  // check if in js is given 'musicURL'
+try {
+    $phpInput = json_decode(file_get_contents('php://input'), true);
+    
+    if ($phpInput === null) {
+        throw new Exception("Невалиден JSON вход.");
+    }
 
-try{
-    $music = saveMusic($connection, $EventID, $title, $musicURL);
+    $db = new DB();
+    $connection = $db->getConnection();
+    $senderID = getSenderID($connection);
+
+    if (!isset($phpInput['eventId'])) {
+        throw new Exception("Липсва необходима информация eventId!");
+    }
+    if (!isset($phpInput['name'])) {
+        throw new Exception("Липсва необходима информация name!");
+    }
+    if (!isset($phpInput['musicURL'])) {
+        throw new Exception("Липсва необходима информация musicURL!");
+    }
+
+    $EventID = $phpInput['eventId']; 
+    $title = $phpInput['name']; 
+    $musicURL = basename($phpInput['musicURL']);
+    
+    $music = new Music(null, $senderID, $title, $musicURL, $EventID);
     $music->storeInDB();
     echo json_encode(['success' => true]);
-} catch(Exception $e) {
+} catch (PDOException $e) {
+    echo json_encode([
+        'success' => false,
+        'message' => "Неуспешно свързване с базата данни!",
+    ]);
+} catch (Exception $e) {
     echo json_encode(['success' => false, 'message' => $e->getMessage()]);
 }
+
