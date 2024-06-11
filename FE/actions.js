1393d5e2cb448d6a27a10f73a7bc7457a8a9ac38
Kaloyan Kateliev <thewolfyplay@gmail.com>
2024-06-11 21:51:30 +0300

    asd

--- FE/actions.js
+++ FE/actions.js
@@ -1,3 +1,68 @@
+function loadEvents() {
+    const section = document.getElementById('events');
+    if(section.innerHTML) {
+        section.innerHTML = '';
+    }
+
+    if(!events.length){
+        let h2 = document.createElement('h2');
+        h2.textContent = 'Няма събития в момента';
+        section.appendChild(h2);
+        return;
+    }
+    //id, title, date, location, isPersonalized(vurni mi imeto ili null), description
+    events.forEach(event => {
+        const mainDiv = document.createElement('div');
+
+        const eventBlockDiv = document.createElement('div');
+        eventBlockDiv.id = event.id;
+        eventBlockDiv.className = "event-block";
+        
+        const titleH3 = document.createElement('h3');
+        titleH3.textContent = event.title;
+        eventBlockDiv.appendChild(titleH3); 
+
+        const dateTimeP = document.createElement('p');
+        dateTimeP.textContent = event.date;
+        eventBlockDiv.appendChild(dateTimeP); 
+
+        const button = document.createElement('button');
+        button.textContent = "Още за събитието";
+        button.onclick = function() {
+            navigateToEvent(event.id); 
+        };
+
+        eventBlockDiv.appendChild(button); 
+
+        mainDiv.appendChild(eventBlockDiv);
+
+        // -----------------------------------
+        var onHoverDiv = document.createElement('div');
+        onHoverDiv.className = "on-hover";
+
+        var titleOrUserH3 = document.createElement('h3');
+        if(event.isPersonalized === "0"){
+            titleOrUserH3.textContent = event.title;
+        }
+        else{
+            titleOrUserH3.textContent = "Празнуващ: " + event.isPersonalized;
+        }
+        onHoverDiv.appendChild(titleOrUserH3); 
+
+        var locationP = document.createElement('p');
+        locationP.textContent = event.location;
+        onHoverDiv.appendChild(locationP);
+
+        var shortDescP = document.createElement('p');
+        shortDescP.textContent = event.description;
+        onHoverDiv.appendChild(shortDescP); 
+
+        mainDiv.appendChild(onHoverDiv);
+
+        section.appendChild(mainDiv); 
+    });
+}
+
 const logoff = () => {
     fetch('../backend/api/logout.php', {
         method: 'POST',
@@ -50,3 +115,6 @@ const isLogged = () => {
 }
 
 isLogged();
+
+
+//get_Global_Events.php
