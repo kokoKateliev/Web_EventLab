cfb0c37a10a6df3e8a009598258c23b5af0f3d1a
Kaloyan Kateliev <thewolfyplay@gmail.com>
2024-06-11 10:53:52 +0300

    asd

--- FE/actions.js
+++ FE/actions.js
@@ -1,30 +1,35 @@
 const logoff = () => {
-    fetch('../../backend/api/logout.php', {
+    fetch('../backend/api/logout.php', {
         method: 'POST',
         body: JSON.stringify(),
     })
     .then(response=>response.json())
     .then(response => {
         if (response.success) {
-            location.replace("../index.html");
+            location.href("index.html");
+        } else {
+            // document.getElementById('user-message').innerText = response.message;
+        }
+    });
+}
+const isLogged = () => {
+    fetch('../backend/api/get_Logged_User.php', {
+        method: 'POST',
+        body: JSON.stringify(),
+    })
+    .then(response=>response.json())
+    .then(response => {
+        if (response.success) {
+            let button = document.getElementById('user');
+            let buttonL = document.getElementById('logout');
+            button.style.display = 'block';
+            buttonL.style.display = 'block';
+            let buttons = document.getElementById('no-user');
+            buttons.style.display = 'none';
         } else {
             // document.getElementById('user-message').innerText = response.message;
         }
     });
 }
 
-fetch('../backend/api/get_Logged_User.php', {
-    method: 'POST',
-    body: JSON.stringify(),
-})
-.then(response=>response.json())
-.then(response => {
-    if (response.success) {
-        let button = document.getElementById('user');
-        button.style.display = 'block';
-        let buttons = document.getElementById('no-user');
-        buttons.style.display = 'none';
-    } else {
-        // document.getElementById('user-message').innerText = response.message;
-    }
-});
+isLogged();
