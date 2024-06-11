f12ebd465fbf59c9f9fe09a19bb60d95bc0cbaee
Adriana Atanasova <adriana0702@abv.bg>
2024-06-11 18:34:35 +0300

    edit save card

--- backend/api/save_Card_DB.php
+++ backend/api/save_Card_DB.php
@@ -5,9 +5,13 @@ session_start();
 require_once "../db/DB.php";
 require_once "../models/Card.php";
 
-function saveCard($connection, $eventId, $name, $photo) {
-    $username = $_SESSION['username'];
+function getSenderID($connection){
+    if (!isset($_SESSION['username'])) {
+        throw new Exception("Няма логнат потребител!");
+    }
 
+    $username = $_SESSION['username'];
+    
     $sql = "SELECT 
                u.id
             FROM 
@@ -18,34 +22,50 @@ function saveCard($connection, $eventId, $name, $photo) {
     $query = $connection->prepare($sql);
     $query->execute([$username]);
 
-    while($row = $query->fetch(PDO::FETCH_ASSOC)){ 
-        $loggedUserID = $row['id'];
-    } 
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+    if ($row === false) {
+        throw new Exception("Потребителят не е намерен.");
+    }
 
-    $card = new Card(null, $loggedUserID, $name, $photo, $eventId);
+    $loggedUserID = $row['id'];
+
+    return $loggedUserID;
 }
 
-$phpInput = json_decode(file_get_contents('php://input'), true);
-$EventID = $phpInput['eventId']; 
-$name = $phpInput['name']; 
-$photo = basename($phpInput['photo']); 
+try {
+    $phpInput = json_decode(file_get_contents('php://input'), true);
+    
+    if ($phpInput === null) {
+        throw new Exception("Невалиден JSON вход.");
+    }
 
-// try{
-//     $db = new DB();
-//     $connetction = $db->getConnection();
-// } catch (PDOException $e) {
-//     echo json_encode([
-//         'success' => false,
-//         'message' => "Неуспешно свързване с базата данни!",
-//         'value' => null,
-//     ]);
-//     exit();
-// }
+    $db = new DB();
+    $connection = $db->getConnection();
+    $senderID = getSenderID($connection);
 
-try{
-    $card = saveCard($connection, $EventID, $name, $photo);
+    if (!isset($phpInput['eventId'])) {
+        throw new Exception("Липсва необходима информация eventId!");
+    }
+    if (!isset($phpInput['name'])) {
+        throw new Exception("Липсва необходима информация name!");
+    }
+    if (!isset($phpInput['photo'])) {
+        throw new Exception("Липсва необходима информация photo!");
+    }
+
+    $eventId = $phpInput['eventId'];
+    $name = $phpInput['name'];
+    $photo = basename($phpInput['photo']);
+
+    $card = new Card(null, $senderID, $name, $photo, $eventId);
     $card->storeInDB();
     echo json_encode(['success' => true]);
-} catch(Exception $e) {
+} catch (PDOException $e) {
+    echo json_encode([
+        'success' => false,
+        'message' => "Неуспешно свързване с базата данни!",
+    ]);
+} catch (Exception $e) {
     echo json_encode(['success' => false, 'message' => $e->getMessage()]);
 }
+?>
