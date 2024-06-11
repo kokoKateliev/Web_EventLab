1a934dd5d68ea0b46fbbe7f56969b1ac0e0cca78
Kaloyan Kateliev <thewolfyplay@gmail.com>
2024-06-11 09:52:10 +0300

    adasd

---
+++ FE/actions.js
@@ -0,0 +1,14 @@
+const logoff = () => {
+    fetch('../backend/api/logout.php', {
+        method: 'POST',
+        body: JSON.stringify(),
+    })
+    .then(response=>response.json())
+    .then(response => {
+        if (response.success) {
+            location.replace("../index.html");
+        } else {
+            document.getElementById('user-message').innerText = response.message;
+        }
+    });
+}
