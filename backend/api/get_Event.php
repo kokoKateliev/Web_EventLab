d9e637856a8f81ca9a6ad9006d5cc95cf8ae6196
Adriana Atanasova <adriana0702@abv.bg>
2024-06-12 21:25:32 +0300

    edit query

--- backend/api/get_Event.php
+++ backend/api/get_Event.php
@@ -29,33 +29,33 @@ function getEvent($connection, $FacultyID) {
     $row1 = $queryID->fetch(PDO::FETCH_ASSOC);
     $loggedUserID = $row1['id'];
 
-    //echo json_encode(['idLogged' => $loggedUserID]);
-
-    $sql = "SELECT DISTINCT
-            e.id,
-            e.EventName,
-            e.EventDateSt,
-            e.Location,
-            e.isAnonymous,
-            e.EventDescription AS description,
-            IF(e.isPersonalized, CONCAT(u.Firstname, ' ', u.Lastname), e.isPersonalized) AS isPersonalized,
-            CONCAT(c.Firstname, ' ', c.Lastname) AS creator,
-            p.isVisible,
-            p.celebratorID
-        FROM 
-            Events e
-        LEFT JOIN 
-            users_events ue ON e.id = ue.EventID
-        LEFT JOIN
-            Personalized p ON p.EventID = e.id
-        LEFT JOIN 
-            Users u ON p.celebratorID = u.id 
-        LEFT JOIN 
-            Users c ON ue.usersID = c.id
-        LEFT JOIN 
-            events_faculties ef ON e.id = ef.EventID
-        WHERE 
-            ef.FacultyID = ?";
+    $sql = "SELECT 
+                e.id,
+                e.EventName,
+                e.EventDateSt,
+                e.Location,
+                e.isAnonymous,
+                e.EventDescription AS description,
+                IF(e.isPersonalized, CONCAT(u.Firstname, ' ', u.Lastname), e.isPersonalized) AS isPersonalized,
+                creatorTable.creator,
+                p.isVisible,
+                p.celebratorID
+            FROM 
+                Events e
+            LEFT JOIN 
+                (SELECT DISTINCT ue.EventID, CONCAT(c.Firstname, ' ', c.Lastname) AS creator
+                FROM users_events ue
+                LEFT JOIN Users c ON ue.usersID = c.id) AS creatorTable ON e.id = creatorTable.EventID
+            LEFT JOIN 
+                Personalized p ON p.EventID = e.id
+            LEFT JOIN 
+                Users u ON p.celebratorID = u.id
+            LEFT JOIN 
+                events_faculties ef ON e.id = ef.EventID
+            WHERE 
+                ef.FacultyID = ?
+            GROUP BY 
+                e.id";
 
         
     $query = $connection->prepare($sql);
@@ -131,18 +131,10 @@ try{
 
 $information = getEvent($connection, $FacultyID);
 
-// if(!$information){
-//     echo json_encode([
-//         'success' => false,
-//         'message' => "Няма намерени данни за събития за избрания факултет.",
-//         'events' => $information,
-//     ]);
-// } else {
-    echo json_encode([
-    'success' => true,
-    'message' => "Намерени са данни за събития към този факултет.",
-    'events' => $information,
-    ]);
-//}
+echo json_encode([
+'success' => true,
+'message' => "Намерени са данни за събития към този факултет.",
+'events' => $information,
+]);
 
 ?>
